7952825aff1e0dfdb21c1344a4e5e470e077c484
Terax235 <weyandsim@gmail.com>
2019-10-06 15:08:34 +0200

    check if cs was updated before getting hotfix

--- structures/API.js
+++ structures/API.js
@@ -6,6 +6,7 @@ import ReadConfig from './API/readHotfix.js'
 const cache = {
   access_token: undefined,
   expiresAt: undefined,
+  cloudstorageVersion: undefined,
   timeout_request: undefined
 }
 
@@ -23,12 +24,7 @@ async function refreshClientToken () {
   const login = await fetch(ENDPOINTS.FORTNITE.OAUTH, {
     method: 'POST',
     body: 'grant_type=client_credentials&token_type=eg1',
-    headers: {
-      'User-Agent': build.fortnite.UserAgent,
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization:
-        'basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE='
-    }
+    headers: { 'User-Agent': build.fortnite.UserAgent, 'Content-Type': 'application/x-www-form-urlencoded', Authorization: 'basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=' }
   }).then(res => res.json())
   cache.access_token = login.token_type + ' ' + login.access_token
   cache.expiresAt = login.expires_at
@@ -46,30 +42,16 @@ export async function getFortniteServerStatus () {
         })
       })
       .then(res => {
-        if (
-          !res ||
-          !res[0] ||
-          !res[0].serviceInstanceId ||
-          res[0].serviceInstanceId !== 'fortnite'
-        ) {
-          return resolve({
-            online: false
-          })
+        if (!res || !res[0] || !res[0].serviceInstanceId || res[0].serviceInstanceId !== 'fortnite') {
+          return resolve({ online: false })
         }
         if (res[0].banned) {
-          return resolve({
-            online: false,
-            banned: true
-          })
+          return resolve({ online: false, banned: true })
         }
         if (res[0].status === 'UP') {
-          return resolve({
-            online: true
-          })
+          return resolve({ online: true })
         }
-        return resolve({
-          online: false
-        })
+        return resolve({ online: false })
       })
   })
 }
@@ -112,10 +94,13 @@ export async function getEncryptionKeys (aes) {
 export async function getHotfix () {
   const token = await refreshClientToken()
   const cloudstorage = await fetch(ENDPOINTS.FORTNITE.CLOUDSTORAGE, { headers: { 'User-Agent': build.fortnite.UserAgent, Authorization: token } }).then(res => res.json())
-  if (!cloudstorage || !cloudstorage[0]) return []
-  if (!cloudstorage.filter(file => file.filename.toLowerCase() === 'defaultgame.ini')[0]) { return [] }
-  const hotfixUri = ENDPOINTS.FORTNITE.CLOUDSTORAGE + '/' + cloudstorage.filter(file => file.filename.toLowerCase() === 'defaultgame.ini')[0].uniqueFilename
+  if (!cloudstorage || !cloudstorage[0]) { return [] }
+  const file = cloudstorage.filter(file => file.filename.toLowerCase() === 'defaultgame.ini')[0]
+  if (!file || typeof file !== 'object' || !file.uniqueFilename) { return [] }
+  if (cache.cloudstorageVersion && file.uploaded && file.uploaded === cache.cloudstorageVersion) { return [] }
+  const hotfixUri = ENDPOINTS.FORTNITE.CLOUDSTORAGE + '/' + file.uniqueFilename
   const hotfix = await fetch(hotfixUri, { headers: { 'User-Agent': build.fortnite.UserAgent, Authorization: token } }).then(res => res.text())
   if (!hotfix) { return [] }
+  if (file.uploaded) { cache.cloudstorageVersion = file.uploaded }
   return hotfix.split('\n').filter(v => v.startsWith('+TextReplacements')).map(v => v.slice(18)).map(v => ReadConfig({ str: v }))
 }
